e9460e5b14608ecdd235d8ce03aa555efcbbd869
Ricardo Garcia <Ricardo@Mac-mini.local>
2023-02-16 18:29:15 -0600

    Maquetacion lista ->Hacienda, ->Habitaciones

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     scrollHeader();
+    menuActive();
 });
 
 function cargandoHome(){
@@ -15,3 +16,18 @@ function scrollHeader(){
     }
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+function menuActive(){
+    const header = document.getElementById("menu_dark");
+
+    // HACIENDA
+    if(document.URL.includes('hacienda.html')){
+        header.classList.add('color_white');
+    }
+
+    // HACIENDA
+    if(document.URL.includes('master-suite.html')){
+        header.classList.add('color_white');
+    }
+}
